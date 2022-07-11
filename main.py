8af5f0f28d3cf34f961b9abb82d2fb556eb81cd8
Nero <14k21096@gmail.com>
2022-07-11 22:00:39 +0300

    Config for centos

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 options = webdriver.ChromeOptions()
 options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
 options.add_argument("--disable-blink-features=AutomationControlled")
-s = Service(executable_path="chromedriver.exe")
+s = Service(executable_path="chromedriver")
 driver = webdriver.Chrome(service=s, options=options)
 
 
