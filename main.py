5e3dfd2d7df9171bd7a7b9f14c4757d992353c4b
Nero <14k21096@gmail.com>
2022-07-12 11:59:04 +0300

    Config for centos

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ options = webdriver.ChromeOptions()
 options.add_argument("user-agent=Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:84.0) Gecko/20100101 Firefox/84.0")
 options.add_argument("--disable-blink-features=AutomationControlled")
 options.add_argument("headless")
-s = Service(executable_path="chromedriver")
+s = Service(executable_path="/root/script/Automation/chromedriver")
 driver = webdriver.Chrome(service=s, options=options)
 
 
